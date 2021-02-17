f542b9318c4b872dbb648bb09d1e1a72a4890f50
Roberto Daniel Torres Cruz <rtorres@cifpfbmoll.eu>
2021-02-17 22:41:26 +0100

    Atributos
    Creación atributos: paraulaSecreta, intents, paraulaEndevinada, intentsInicials

--- com/jaume/penjat/Tauler.java
+++ com/jaume/penjat/Tauler.java
@@ -1,4 +1,8 @@
 package com.jaume.penjat;
 
 public class Tauler {
+	private char[] paraulaSecreta;
+    private int intents;
+    private String[] paraulaEndevinada;
+    private int intentsInicials;
 }
