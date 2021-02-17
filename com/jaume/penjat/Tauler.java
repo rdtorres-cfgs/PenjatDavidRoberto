d9d4e5bdb7b7e8e8320ba3ae92a4d343cb3c7e66
David <dsastref@cifpfbmoll.eu>
2021-02-17 22:46:51 +0100

    Getters&Setters
    Creamos todos los getters y setters de los atributos.

--- com/jaume/penjat/Tauler.java
+++ com/jaume/penjat/Tauler.java
@@ -5,4 +5,36 @@ public class Tauler {
     private int intents;
     private String[] paraulaEndevinada;
     private int intentsInicials;
+
+    public char[] getParaulaSecreta() {
+        return paraulaSecreta;
+    }
+
+    public void setParaulaSecreta(char[] paraulaSecreta) {
+        this.paraulaSecreta = paraulaSecreta;
+    }
+
+    public int getIntents() {
+        return intents;
+    }
+
+    public void setIntents(int intents) {
+        this.intents = intents;
+    }
+
+    public String[] getParaulaEndevinada() {
+        return paraulaEndevinada;
+    }
+
+    public void setParaulaEndevinada(String[] paraulaEndevinada) {
+        this.paraulaEndevinada = paraulaEndevinada;
+    }
+
+    public int getIntentsInicials() {
+        return intentsInicials;
+    }
+
+    public void setIntentsInicials(int intentsInicials) {
+        this.intentsInicials = intentsInicials;
+    }
 }
